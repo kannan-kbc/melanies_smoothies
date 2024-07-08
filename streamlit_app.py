289dd50870bf56f812639011017829b54c922844
kannan-kbc <kannanruleup@gmail.com>
2024-07-08 17:22:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,3 +51,8 @@ if time_to_insert:
 
         st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
 
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
